e62179e3f7b9a4ec71598ce637b21da24f3ccbe4
stijstev <stijstev@student.arteveldehs.be>
2017-10-05 10:47:34 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    $string1 = "Hello";
+    $string2 = "World";
+    $strings = "{$string1} {$string2}";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Name</title>
+</head>
+<body>
+    <h1>These strings are concatenated! : <?=$strings?></h1>
+</body>
+</html>
